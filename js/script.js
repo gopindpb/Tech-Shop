51f37b46ad7a8f518fba9c4f8dd4d48616f98778
gopindpb <gopindpb05@gmail.com>
2026-08-17 22:09:05 +0530

    fetched and displayed data on products page

--- js/script.js
+++ js/script.js
@@ -1,5 +1,58 @@
+
+//Navbar
 fetch("navbar.html")
     .then(response => response.text())
     .then(data => {
         document.getElementById("navbar").innerHTML = data;
     });
+
+
+// Fetch products from API
+let Allproducts = [];
+async function getAllProducts() {
+    try {
+        let response = await fetch("https://fakestoreapi.com/products");
+        let products = await response.json();
+        console.log(products);
+        Allproducts = products;
+        displayProducts(products)
+    }
+    catch (error) {
+        console.log(error);
+
+    }
+}
+getAllProducts();
+// Products 
+
+function displayProducts(products) {
+
+    const ProductsContainer = document.querySelector("#products");
+
+    ProductsContainer.innerHTML = "";
+    
+
+    products.forEach((product) => {
+
+        ProductsContainer.innerHTML += `
+
+        <div class="card">
+            <img src="${product.image}" class="card-img-top" alt="${product.title}">
+            <div class="card-body">
+                <h5 class="product-title">${product.title}</h5>
+                <p class="product-text">${product.description}</p>
+            </div>
+            <ul class=" price list-group list-group-flush">
+                <li class="list-group-item">$ ${product.price}</li>
+            </ul>
+            <div class="product-card-body">
+                <button class="product-details">Details</button>
+                <button class="product-Cart">Cart</button>
+            </div>
+        </div>
+        `
+    })
+}
+
+
+
